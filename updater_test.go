48e55018c150d6a69f0c22fe7c50cadf8370c8bc
Cliffano Subagio <cliffano@gmail.com>
2016-03-13 21:31:13 +1100

    Update test to reflect multi-region revert.

--- updater_test.go
+++ updater_test.go
@@ -2,31 +2,21 @@ package main
 
 import (
 	"bytes"
-	"github.com/mitchellh/packer/packer"
 	"testing"
+
+	"github.com/mitchellh/packer/packer"
+	"github.com/stretchr/testify/assert"
 )
 
-// This is totally not isolated from the filesystem
-func TestUpdateJsonFileWithString(t *testing.T) {
-	file := "/tmp/testpathstring.json"
-	paths := []string{"testpathstring"}
+func TestUpdateJSONFileShouldGiveErrorWhenFileDoesNotExist(t *testing.T) {
+	file := "testdata/some_inexisting_file.json"
+	paths := []string{"some.path.foo.bar"}
 	ui := &packer.BasicUi{
 		Reader:      new(bytes.Buffer),
 		Writer:      new(bytes.Buffer),
 		ErrorWriter: new(bytes.Buffer),
 	}
-	value := "simplevalue_string"
-	_ = UpdateJsonFile(file, paths, value, ui, true)
-}
-
-func TestUpdateJsonFileWithMap(t *testing.T) {
-	file := "/tmp/testpathmap.json"
-	paths := []string{"testpathmap"}
-	ui := &packer.BasicUi{
-		Reader:      new(bytes.Buffer),
-		Writer:      new(bytes.Buffer),
-		ErrorWriter: new(bytes.Buffer),
-	}
-	value := map[string]string{"us-east-1": "ami-123456"}
-	_ = UpdateJsonFile(file, paths, value, ui, true)
+	value := "ami-12345"
+	err := UpdateJSONFile(file, paths, value, ui)
+	assert.Equal(t, err.Error(), "open testdata/some_inexisting_file.json: no such file or directory")
 }
